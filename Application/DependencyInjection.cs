3b1532bc1b9c0f2c129c4db1e056140a9e6f85cc
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-10-27 16:56:57 +0300

    added mediator

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using static Application.Features.WorkplaceBookingFeatures.Queries.GetAllWorkplaceBookingQuery;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
