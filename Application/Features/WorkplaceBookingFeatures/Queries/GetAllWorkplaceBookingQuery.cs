9be6aa7d899917722695fde1d3d4c97fb5c8a71d
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-10-27 16:57:07 +0300

    created query

---
+++ Application/Features/WorkplaceBookingFeatures/Queries/GetAllWorkplaceBookingQuery.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.WorkplaceBookingFeatures.Queries
+{
+    public class GetAllWorkplaceBookingQuery : IRequest<IEnumerable<WorkplaceBooking>>
+    {
+        public class GetAllWorkplaceBookingQueryHandler : IRequestHandler<GetAllWorkplaceBookingQuery, IEnumerable<WorkplaceBooking>>
+        {
+            private readonly IUnitOfWork _unitOfWork;
+            public GetAllWorkplaceBookingQueryHandler(IUnitOfWork unitOfWork)
+            {
+                _unitOfWork = unitOfWork;
+            }
+            public async Task<IEnumerable<WorkplaceBooking>> Handle(GetAllWorkplaceBookingQuery query, CancellationToken cancellationToken)
+            {
+                var workplaceBookingsList = await _unitOfWork.WorkplaceBookings.GetAll();
+                return workplaceBookingsList;
+            }
+        }
+    }
+}
