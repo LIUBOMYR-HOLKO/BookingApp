cf7551cd3dba6c591f9f51e70f8f787f4bfa2361
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-10-27 16:58:43 +0300

    created controllers

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator
+        {
+            get => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        }
+    }
+}
---
+++ API/Controllers/WorkplaceBookingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Application.Features.WorkplaceBookingFeatures.Queries;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkplaceBookingController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllWorkplaceBookingQuery()));
+        }
+    }
+}
