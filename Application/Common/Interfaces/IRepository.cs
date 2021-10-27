a149fbff6bedcd0854c514adc5a6ea5d3576e58a
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-10-27 16:54:01 +0300

    added interfaces for repository and UoW

---
+++ Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task Create(T item);
+        Task Delete(int id);
+        IEnumerable<T> Find(Func<T, bool> predicate);
+        Task<T> Get(int id);
+        Task<IEnumerable<T>> GetAll();
+        void Update(T item);
+    }
+}
