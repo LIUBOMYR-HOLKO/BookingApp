6f9be611cb6405cde8c8fa4571ac5490c92d6a62
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-10-27 16:54:24 +0300

    Implamented interfaces

---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private WorkplaceBookingRepository _workplaceBookingRepository;
+        private bool disposed = false;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public IRepository<WorkplaceBooking> WorkplaceBookings
+        {
+            get
+            {
+                if (_workplaceBookingRepository == null)
+                {
+                    _workplaceBookingRepository = new WorkplaceBookingRepository(_context);
+                }
+                return _workplaceBookingRepository;
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/WorkplaceBookingRepository.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Entities;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Persistence.Repositories
+{
+    class WorkplaceBookingRepository : IRepository<WorkplaceBooking>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WorkplaceBookingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(WorkplaceBooking item)
+        {
+            await _context.WorkplaceBookings.AddAsync(item);
+        }
+
+        public async Task Delete(int id)
+        {
+            var item = await _context.WorkplaceBookings.FindAsync(id);
+            _context.WorkplaceBookings.Remove(item);
+        }
+
+        public IEnumerable<WorkplaceBooking> Find(Func<WorkplaceBooking, bool> predicate)
+        {
+            return _context.WorkplaceBookings.Where(predicate).ToList();
+        }
+
+        public async Task<WorkplaceBooking> Get(int id)
+        {
+            return await _context.WorkplaceBookings.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<WorkplaceBooking>> GetAll()
+        {
+            return await _context.WorkplaceBookings.ToListAsync();
+        }
+
+        public void Update(WorkplaceBooking item)
+        {
+            _context.WorkplaceBookings.Update(item);
+        }
+    }
+}
